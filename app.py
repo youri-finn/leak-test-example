e8011f25135f23b3d5427e6b90be325e48654441
youri-finn <youri.penders@live.nl>
2025-01-29 16:55:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    first_name = 'Youri'
+    last_name = 'Penders'
+    today = date.today()
+    return render_template('index.html', prenom = first_name, nom = last_name, vandaag = today)
+
+if __name__ == '__main__':
+    app.run()
