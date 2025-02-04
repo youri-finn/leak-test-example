124a125778c6093a1be278f55b365e875a231b8d
youri-finn <youri.penders@live.nl>
2025-02-04 16:18:55 +0100

    Updated with 'file upload', average calculation and plot display

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template, send_file
+import pandas as pd
+import io
+import matplotlib.pyplot as plt
 from datetime import date
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+
+    average = None
+
     first_name = 'Youri'
     last_name = 'Penders'
     today = date.today()
-    return render_template('index.html', prenom = first_name, nom = last_name, vandaag = today)
+
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['file']
+
+        if file.filename == "":
+            return jsonify({'error': 'No selected file'}), 400
+
+        if file.filename.endswith(('.xls', '.xlsx')):
+            global df
+            df = pd.read_excel(file)
+        else:
+            return jsonify({'error': 'Unsupported file format'}), 400
+
+        average = df.Temperature.mean()
+
+    return render_template('index.html', prenom=first_name, nom=last_name, vandaag=today, average=average)
+
+@app.route('/plot')
+def decay_plot():
+
+    plt.figure()
+    plt.plot(df.Date, df.Temperature, label='Temperature')
+    plt.plot(df.Date, df.Pressure, label='Pressure')
+    plt.xlabel('Date')
+    plt.ylabel('Temperature / Pressure')
+    plt.title('Pressure Decay Test Plot')
+    plt.legend()
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight')
+    img.seek(0)
+
+    return send_file(img, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)#host="0.0.0.0", port=8080)
