79a2fe9845b3ecbbc3d00d8a51fb1a06f6e4cfb9
youri-finn <youri.penders@live.nl>
2025-02-04 16:21:34 +0100

    Updated with 'file upload', average calculation and plot display

--- backend/app.py
+++ backend/app.py
@@ -53,4 +53,4 @@ def decay_plot():
     return send_file(img, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)#host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
